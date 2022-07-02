ecacc96576980a894f391be2bd31bcbd53b91eed
Travis Howse <tjhowse@gmail.com>
2022-07-02 16:46:11 +1000

    Remove open loop waits.

--- refresh_cookies.py
+++ refresh_cookies.py
@@ -1,5 +1,6 @@
 #!/usr/local/bin/python3
 import json
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -24,7 +25,7 @@ def log_in_and_get_cookies(driver_url, username="", password=""):
     chrome_options = webdriver.ChromeOptions()
     chrome_options.set_capability("browserVersion", "103")
     # Set the resolution to 1280x1024 so we don't get the mobile version of the page
-    chrome_options.add_argument("--window-size=1280,1024")
+    chrome_options.add_argument("--window-size=1600,1024")
     driver = webdriver.Remote(
         command_executor=f"{driver_url}/wd/hub", options=chrome_options
     )
@@ -39,8 +40,11 @@ def log_in_and_get_cookies(driver_url, username="", password=""):
     # Reload the page with cookies loaded
     f = driver.get("https://www.woolworths.com.au")
 
-    # Check driver.page_source for indication that we're logged out. Log in if required.
-    if "Log in / Signup" in driver.page_source:
+    # Check the logged in indicator to determine whether we need to log in.
+    login_indicator = WebDriverWait(driver, 5).until(
+        lambda x: x.find_element(by=By.XPATH, value="/html/body/div[2]/div[2]/section/button[2]/div/span")
+    )
+    if "My Account" not in login_indicator.text:
         print("We're logged out, we need to log in.")
         try:
             login_button = WebDriverWait(driver, 5).until(
@@ -58,15 +62,21 @@ def log_in_and_get_cookies(driver_url, username="", password=""):
             remember_me_tick.click()
             username_box.send_keys(username)
             password_box.send_keys(password + Keys.ENTER)
+
+            # Wait until we're logged in
+            timeout = time.time() + 5
+            while time.time() < timeout:
+                if "My Account" in login_indicator.text:
+                    break
+                time.sleep(0.1)
+            else:
+                print("Failed to refresh cookies.")
         except Exception as e:
             # Save a screenshot of the page
             driver.save_screenshot("login_error.png")
             with open("page.html", "w") as f:
                 f.write(driver.page_source)
             raise (e)
-
-    # TODO Replace this with something that confirms we're logged in
-    driver.implicitly_wait(2)
 
     # Convert the cookies to a simple dict for saving.
     for cookie in driver.get_cookies():
